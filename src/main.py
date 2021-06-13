1a7948cdc8df6d477b21d0ffd62847c5771e5b26
Filip Aronshtein <47334339+aronshteinf@users.noreply.github.com>
2021-06-12 22:46:57 -0400

    Fixed bugs in initial implementation.
    Accuracy on train/test:
    2 qubits: 1.0 / 1.0
    3 qubits: 0.978125 / 0.9775
    4 qubits: 0.99875 / 0.995

--- src/main.py
+++ src/main.py
@@ -28,7 +28,7 @@ def logistic_regression(X_train, X_test, y_train, y_test):
     return train_preds, test_preds
 
 
-def run_qks(p, number_of_qubits, qc, n_episodes, scale, n_trials):
+def run_qks(p, number_of_qubits, n_episodes, scale, n_trials):
     """
     Parameters:
     p : int, default = None
@@ -55,45 +55,18 @@ def run_qks(p, number_of_qubits, qc, n_episodes, scale, n_trials):
     """
 
     r = 1 if p / number_of_qubits < 1 else int(p / number_of_qubits)
-    QKS = QuantumKitchenSinks(qc, n_episodes=n_episodes,
+    QKS = QuantumKitchenSinks(n_episodes=n_episodes,
                               r=r, scale=scale,
                               distribution='normal',
-                              n_trials=n_trials)
+                              n_trials=n_trials, p=p, q=number_of_qubits)
     return QKS
 
 
 def make_plot(data, target, name, title):
     plt.figure(figsize=(5, 5))
     plt.title(title)
-    plt.scatter(data[:0], data[:1], s=5, c=target)
+    plt.scatter(data[:,0], data[:,1], s=5, c=target)
     plt.savefig(name)
-
-    qc = make_qc(q, noisy)
-    p = X_train.shape[-1]
-
-    QKS = run_qks(p, 2, qc, 1000, 1, 1000)
-
-    QKS_train = QKS.fit_transform(X_train)
-    QKS_test = QKS.fit_transform(X_test)
-
-    train_preds, test_preds = logistic_regression(QKS_train, QKS_test, y_train, y_test)
-
-    plot = True
-    img_path = 'figs/'
-
-    if plot is True:
-        make_plot(X_train, y_train, img_path + 'training_dataset', 'Training Set')
-        make_plot(X_test, y_test, img_path + 'test_dataset', 'Test Set')
-        make_plot(X_train, train_preds, img_path + 'results_experiment_train', 'Experiment Results - Training Dataset')
-        make_plot(X_test, test_preds, img_path + 'results_experiment_test', 'Experiment Results - Test Dataset')
-
-#def make_qc(q, noisy=False):
-#    q_str = str(q) + 'q'
-#    qvm = '-qvm'
-#    name = q_str + qvm if not noisy else q_str + '-noisy' + qvm
-#    print("name of qc: ", name)
-#    qc = get_qc(name)
-#    return qc
 
 def main():
     X_train, X_test, y_train, y_test = make_frames(
@@ -104,20 +77,33 @@ def main():
     scale = 1
     n_trials = 1000
     p = X_train.shape[-1]
-    q = 2
-    #noisy = False
-    qc = QuantumCircuit(q)
+    q = 4
 
-    run_qks(p, q, qc, n_episodes, scale, n_trials)
+    QKS = run_qks(p, q, n_episodes, scale, n_trials)
+
+    QKS_train = QKS.fit_transform(X_train)
+    QKS_test = QKS.fit_transform(X_test)
+    print("Quantum Part Done")
+    train_preds, test_preds = logistic_regression(QKS_train, QKS_test, y_train, y_test)
+
+    plot = True
+    img_path = 'fig/'
+
+    if plot is True:
+        #print(X_train)
+        #print(y_train)
+        make_plot(X_train, y_train, img_path + 'training_dataset', 'Training Set')
+        make_plot(X_test, y_test, img_path + 'test_dataset', 'Test Set')
+        make_plot(X_train, train_preds, img_path + 'results_experiment_train', 'Experiment Results - Training Dataset')
+        make_plot(X_test, test_preds, img_path + 'results_experiment_test', 'Experiment Results - Test Dataset')
 
 # Write the API token to IBM Q
-my_api_token = ""
+my_api_token = "f84c8a32633b1ce7fda84ebc53c33141d96526678b3eba6ed5709619cb2d7378a0770a72831f01ded4dd4a9ce691cfc5042e3c622b03129ce5376c5f000b5dcf"
 IBMQ.save_account(my_api_token)
-IBMQ.load_accounts()
 
 # Check the connection for IBM Q
 try:
-    IBMQ.load_accounts()
+    IBMQ.load_account()
 except:
     print("""WARNING: There's no connection with the API for remote backends.
              Have you initialized a file with your personal token?
